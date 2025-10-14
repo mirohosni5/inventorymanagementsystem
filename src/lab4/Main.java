f65378f7cf4e78806bb072c492f18cb504028268
mayar <mirohosni5@gmail.com>
2025-10-15 01:21:51 +0300

    auto: src\lab4\Main.java

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -6,7 +6,7 @@ package lab4;
 
     public class Main {
         public static void main(String[] args) {
-            System.out.println("App is running heello world ✅"); //hi there there bb bb bbbbb
+            System.out.println("App is running heello worldbb ✅"); //hi there there bb bb bbbbb
 
         }
     }
