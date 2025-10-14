cb221a32cc38967565aa9a01d1ec006ed8e16a05
mayar <mirohosni5@gmail.com>
2025-10-15 01:59:38 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -57,7 +57,7 @@ public class EmployeeUser {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-public static boolean checkId(String idCheck,List<>)
+public static boolean checkId(String idCheck,List<EmployeeUser>)
     //methods
     public String lineRepresentation() {
         return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
