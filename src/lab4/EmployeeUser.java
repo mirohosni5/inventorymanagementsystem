2fec4067ab6bdcd7014c9297160c4f88f4565f52
mayar <mirohosni5@gmail.com>
2025-10-15 01:57:49 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -1,56 +1,72 @@
 package lab4;
 import java.util.*;
 public class EmployeeUser {
-private String employeeId;
-private String name;
-private String email;
-private String address;
-private String phoneNumber;
-//constructor
-public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
-    this.employeeId = employeeId;
-    this.name = name;
-    this.email = email;
-    this.address = address;
-    this.phoneNumber = phoneNumber;
-}
-//getters
-public String getEmployeeId() {
-    return employeeId;}
+    private String employeeId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    //constructor
+    public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //getters
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
     public String getName() {
-        return employeeId;}
+        return employeeId;
+    }
+
     public String getEmail() {
-        return employeeId;}
+        return employeeId;
+    }
+
     public String getAddress() {
-        return employeeId;}
+        return employeeId;
+    }
+
     public String getPhoneNumber() {
-        return employeeId;}
-//setters
-public void setEmployeeId(String employeeId) {
-    this.employeeId = employeeId;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public void setAddress(String address) {
-    this.address = address;
-}
-public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-}
-//methods
-    public String lineRepresentation(){
-    return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;}
-    public String getSearchKey(){
-    return employeeId;}
+        return employeeId;
+    }
 
+    //setters
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    //methods
+    public String lineRepresentation() {
+        return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
+    }
+
+    public String getSearchKey() {
+        return employeeId;
+    }
+}
 
 
 
