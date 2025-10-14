b721a797d908febfdd54900eafc7874f2a467b43
mayar <mirohosni5@gmail.com>
2025-10-15 02:08:31 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -59,7 +59,7 @@ public class EmployeeUser {
     }
 public static boolean checkId(String idCheck,List<EmployeeUser> employes){
 for (EmployeeUser emp : employes){
-    if (idCheck !=null&&idCheck.equals(emp.getSearchKey())){
+    if (idCheck !=null&&idCheck.equalsIgnoreCase(emp.getSearchKey())){
         return true;
     }
 
