b0a4f2927c5bb2be6c67e5fd05ddcb217a7445fe
mayar <mirohosni5@gmail.com>
2025-10-14 14:23:33 +0300

    second commit

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -6,7 +6,7 @@ package lab4;
 
     public class Main {
         public static void main(String[] args) {
-            System.out.println("App is running ✅"); //hi
+            System.out.println("App is running ✅"); //hi there
 
         }
     }
