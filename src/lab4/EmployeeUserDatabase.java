5a4b5423087e51bfc66d8d9a7205fb6864903dd0
mayar <mirohosni5@gmail.com>
2025-10-15 18:18:44 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -52,7 +52,7 @@ public class EmployeeUserDatabase {
         String email=data[2].trim();
         String address=data[3].trim();
         String phonenumber=data[4].trim();
-        return new EmployeeUser(employeeid,name,email,address,phonenumber);
+        retur new EmployeeUser(employeeid,name,email,address,phonenumber);
     }
 
     public boolean contains(String key){
