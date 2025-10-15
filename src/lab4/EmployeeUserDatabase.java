6801cbceb56985e341a8c31c15a5c03c89bd0446
mayar <mirohosni5@gmail.com>
2025-10-15 16:22:39 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -51,7 +51,9 @@ return new EmployeeUser(employeeid,name,email,address,phonenumber);
                 }
             }
         }
-        
+        catch (IOException e){
+            System.out.println("Error reading file");
+        }
 
 
     }
