3ae5c69df2bf6eb7c120f20382fb4d91364b587a
mayar <mirohosni5@gmail.com>
2025-10-15 17:18:52 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -57,7 +57,7 @@ return new EmployeeUser(employeeid,name,email,address,phonenumber);
     public boolean contains(String key){
         return EmployeeUser.isIDTaken(key,records);
     }
-    public EmployeeUser getRecord(String key){//searching for the record an
+    public EmployeeUser getRecord(String key){//searching for the record usi
          if (contains(key)){
              for (EmployeeUser emp:records){
                  if (emp.getEmployeeId().equalsIgnoreCase(key)){
