7d715c4fe0fec46f35b2226981205774e2c21b91
mayar <mirohosni5@gmail.com>
2025-10-15 16:34:16 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -59,7 +59,7 @@ public class EmployeeUser {
     }
 
     //methods
-    public  boolean contain
+    public  boolean contains(string k)
     public String lineRepresentation() {
         return String.join(",",employeeId,name,email,address,phoneNumber);
     }
