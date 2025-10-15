23a9ddf23256b5fd288984ae645c9a2e20c9e783
mayar <mirohosni5@gmail.com>
2025-10-15 15:50:26 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -11,7 +11,7 @@ public class EmployeeUserDatabase {
        this.records=new ArrayList<>();
     }
     public EmployeeUser createRecordFrom(String line){
-        String[] tokens=line.split([]);
+        String[] tokens=line.split();
         String name=tokens[0];
         String email=tokens[1];
         String ssn=tokens[2];
