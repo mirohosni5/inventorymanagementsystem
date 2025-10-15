61c28503f85dc4f36cfa3aebe7cf0635b15b4892
mayar <mirohosni5@gmail.com>
2025-10-15 17:11:01 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -12,7 +12,7 @@ public class EmployeeUserDatabase {
     }
     public EmployeeUser createRecordFrom(String line){
         String[] data=line.split("," );
-        if (data.length==5){
+        data.length==5
             return null;
         }
         String employeeid=data[0].trim();
