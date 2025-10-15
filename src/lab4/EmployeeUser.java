14fb1545137a16c02eabd5f22a18d7d5ebcde578
mayar <mirohosni5@gmail.com>
2025-10-15 16:42:44 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -73,7 +73,7 @@ public class EmployeeUser {
         return String.join(",",employeeId,name,email,address,phoneNumber);
     }
 
-    public String getSearchKey(List<EmployeeUser>employeeUsers) {
+    public String getSearchKey(List<EmployeeUser>employees) {
         EmployeeUser.isIDTaken()
 
         return employeeId;
