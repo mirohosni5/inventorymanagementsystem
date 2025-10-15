275bacc74fe9c9ff9a4ecaf725113068c0a523b8
mayar <mirohosni5@gmail.com>
2025-10-15 16:35:57 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -73,7 +73,7 @@ public class EmployeeUser {
         return String.join(",",employeeId,name,email,address,phoneNumber);
     }
 
-    public String getSearchKey(List<Em) {
+    public String getSearchKey() {
 
         return employeeId;
     }
