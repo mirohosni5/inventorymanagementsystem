c4c012a4f3f682418362bcf585c17dab2f7475ee
mayar <mirohosni5@gmail.com>
2025-10-18 00:50:35 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -79,7 +79,7 @@ public abstract class databases<T extends IDtkenRecord> {
         System.out.println("record added"+ record.getSearchKey());
         saveToFile();
         }
-        public 
+        public void deleteRec
 
     }
 
