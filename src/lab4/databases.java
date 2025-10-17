2a2ea3d11276374b6e689f43f48ebb4d1aad2bc4
mayar <mirohosni5@gmail.com>
2025-10-17 06:56:53 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -58,7 +58,7 @@ public abstract class databases<T> {
                         return true;
                     }
                     else if (record instanceof Product){
-                        if(((Product) record).)
+                        if(((Product) record).getP)
                     }
                 }
 
