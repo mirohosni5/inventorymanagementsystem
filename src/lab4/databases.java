ae39b951257c3973d6f92fcc91bbf439761ca7b1
mayar <mirohosni5@gmail.com>
2025-10-17 07:02:32 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -54,6 +54,7 @@ public abstract class databases<T> {
     public boolean contains(String key){
             for( T record:records){
                 if ( record instanceof EmployeeUser){
+                    
                     if(emp.getEmployeeId().equalsIgnoreCase(key)){
                         return true;
                     }
