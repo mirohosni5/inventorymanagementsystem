1750b7ab4cd59f5b80565cfb4ff18c61fb5f6171
mayar <mirohosni5@gmail.com>
2025-10-17 06:41:08 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -54,7 +54,7 @@ public abstract class databases<T> {
        try{  BufferedWriter bw= new BufferedWriter(new FileWriter(filename));
            for(T record:records){
                if (record instanceof EmployeeUser){
-                   
+                   pw.prin
                }
            }
 
