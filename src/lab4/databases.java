e33226a5f9cd9946ed84c0bf6f5cb3d7cf5b95d6
mayar <mirohosni5@gmail.com>
2025-10-17 06:16:29 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -23,7 +23,7 @@ public abstract class databases<T> {
         //reading the file
     try{
         String line;
-        while (line=br.readLine()!=null)
+        while (line=br.readLine())!=null)
     }
 
 
