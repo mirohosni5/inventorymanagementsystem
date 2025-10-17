f21d360ea0b0c72fdd17cf643ea5ae22049217df
mayar <mirohosni5@gmail.com>
2025-10-17 23:22:04 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -1,6 +1,6 @@
 package lab4;
 import java.util.*;
-public class EmployeeUser {
+public class EmployeeUser implements  {
     private String employeeId;
     private String name;
     private String email;
