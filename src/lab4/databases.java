beb12837d9c2474d4be590440e509ab44b2fe83b
mayar <mirohosni5@gmail.com>
2025-10-18 00:28:08 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -53,7 +53,7 @@ public abstract class databases<T extends IDtkenRecord> {
         return records;
     }
     public boolean contains(String key){
-        return IDtkenRecord.isIDtaken(key)
+        return IDtkenRecord.isIDtaken(key,records);
     }
     public T getRecord(String key){
         if(!contains(key)){
