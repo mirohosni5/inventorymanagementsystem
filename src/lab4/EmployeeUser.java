c88845a40f7eda8046eb790638bfb9c387e3dc10
mayar <mirohosni5@gmail.com>
2025-10-17 23:44:04 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -1,6 +1,6 @@
 package lab4;
 import java.util.*;
-public class EmployeeUser implem {
+public class EmployeeUser  {
     private String employeeId;
     private String name;
     private String email;
