b5fc44f8477bbdd8324c9392025bc26f49a64398
mayar <mirohosni5@gmail.com>
2025-10-18 01:00:33 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -1,7 +1,7 @@
 package lab4;
 import java.io.*;
 import java.util.*;
-//here i used generic 
+//here i used generic so it would take any
 public abstract class databases<T extends IDtkenRecord> {
     public ArrayList<T> records= new ArrayList<>();
     public String filename;
