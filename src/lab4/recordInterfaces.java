2daae3f8abeb1ada00edfdf136763ac7fbefa44a
mayar <mirohosni5@gmail.com>
2025-10-17 23:28:01 +0300

    auto: src\lab4\recordInterfaces.java

--- src/lab4/recordInterfaces.java
+++ src/lab4/recordInterfaces.java
@@ -1,5 +1,5 @@
 package lab4;
-//this will be implemented a
+//this will be implemented ain all the classes this means the t
 public interface recordInterfaces {
     String lineRepresentation();
     String getSearchKey();
