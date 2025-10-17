bf0ee7c77dd4c96513a0fb7b3a8975089e26cbbe
mayar <mirohosni5@gmail.com>
2025-10-18 00:48:27 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -71,7 +71,9 @@ public abstract class databases<T extends IDtkenRecord> {
             return;
 
         }
-        if(contains(record.getSearchKey())
+        if(contains(record.getSearchKey())){
+            
+        }
         }
 
     }
