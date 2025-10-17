eac58ec5d264abb5d8772829df184770a3ce98df
mayar <mirohosni5@gmail.com>
2025-10-17 06:46:45 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -55,7 +55,7 @@ public abstract class databases<T> {
            for(T record:records){
                if (record instanceof EmployeeUser) {
                    bw.write(((EmployeeUser) record).lineRepresentation());
-               } else if (record instanceof Product) { //i used
+               } else if (record instanceof Product) { //i used instanceof here to check object t
                    bw.write(((Product) record).lineRepresentation());))
                    
                }
