c33197fa53b359b3394ecf97d495f0f72d529c19
mayar <mirohosni5@gmail.com>
2025-10-18 00:04:43 +0300

    auto: src\lab4\IDtkenRecord.java

--- src/lab4/IDtkenRecord.java
+++ src/lab4/IDtkenRecord.java
@@ -1,7 +1,7 @@
 package lab4;
 import java.util.*;
 /*this will be the parent class that the rest of the classes (that will need unique id) will inherit from it also has the implementation so u dont need to o implement too
-  any other class that has search key and line representation use the interf
+  any other class that has search key and line representation use the interface class 3adi
      */
 public abstract class IDtkenRecord  implements recordInterfaces{
     //to check for same ids
