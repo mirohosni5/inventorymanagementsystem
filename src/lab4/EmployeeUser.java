ee23d33156c4b36a2421542d7e49f34598bf528a
mayar <mirohosni5@gmail.com>
2025-10-17 23:43:55 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -1,6 +1,6 @@
 package lab4;
 import java.util.*;
-public class EmployeeUser implements recordInterfaces {
+public class EmployeeUser implem {
     private String employeeId;
     private String name;
     private String email;
