39cc25d2e64a8f0db096fdf05ec3d544b8119e2b
mayar <mirohosni5@gmail.com>
2025-10-17 23:54:02 +0300

    auto: src\lab4\EmployeeUser.java

--- src/lab4/EmployeeUser.java
+++ src/lab4/EmployeeUser.java
@@ -1,6 +1,6 @@
 package lab4;
 import java.util.*;
-public class EmployeeUser extends   {
+public class EmployeeUser extends IDtkenRecord  {
     private String employeeId;
     private String name;
     private String email;
