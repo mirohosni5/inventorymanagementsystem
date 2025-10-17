2e2fa25e1825bfa8e74b1c1835c370d48542764d
mayar <mirohosni5@gmail.com>
2025-10-17 06:13:52 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -14,6 +14,8 @@ public abstract class databases<T> {
         try{ br=new BufferedReader(new FileReader(filename));
 
     }
-        catch (FileNotFoundException e)
+        catch (FileNotFoundException e){
+            
+        }
 
 }}
