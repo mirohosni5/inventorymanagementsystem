e9868ad7ea082663f2e4fb71680ac97b48d51081
mayar <mirohosni5@gmail.com>
2025-10-17 06:40:13 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -52,7 +52,7 @@ public abstract class databases<T> {
     }
     public void savetoFile(){
        try{  BufferedWriter bw= new BufferedWriter(new FileWriter(filename));
-           for(T re)
+           for(T reocrd:records)
 
     }
 
