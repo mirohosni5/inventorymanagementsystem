04069989453de5673935154fcce433b3525b0ff3
mayar <mirohosni5@gmail.com>
2025-10-18 00:52:50 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -80,7 +80,7 @@ public abstract class databases<T extends IDtkenRecord> {
         saveToFile();
         }
         public void deleteRecord(String key){
-        T =getRecord(key);
+        T i=getRecord(key);
         if(!contains(key)){
             System.out.println("id isnt there");
         }
