fbeb8ff856f03a1fb37cf0f24261df6652d31596
mayar <mirohosni5@gmail.com>
2025-10-17 23:52:05 +0300

    auto: src\lab4\IDtkenRecord.java

--- src/lab4/IDtkenRecord.java
+++ src/lab4/IDtkenRecord.java
@@ -8,7 +8,8 @@ public abstract class IDtkenRecord  implements recordInterfaces{
                        return true;}
 
                    }
+               
                }
-               return false;
+
     }
 
