01d19b464a34098e0198f4939cae71d67b4f2f9d
mayar <mirohosni5@gmail.com>
2025-10-17 07:03:50 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -59,7 +59,7 @@ public abstract class databases<T> {
                         return true;
                     }
                     else if (record instanceof Product){
-                        Product prod=
+                        Product prod=(Product) record;
                         if(prod.getProductId().equalsIgnoreCase(key)){
                             return true;
                         }
