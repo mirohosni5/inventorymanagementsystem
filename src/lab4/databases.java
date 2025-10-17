87daeaac8f55e6aaba8ac3c2fd7ddaf977c4d5d7
mayar <mirohosni5@gmail.com>
2025-10-17 06:12:47 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -10,7 +10,7 @@ public abstract class databases<T> {
     }
     public void readFromFile(){
         records.clear();
-        //to clear anything inside record first
+        BufferedReader//to clear anything inside record first
         try{ br=new BufferedReader(new FileReader(filename));
 
     }
