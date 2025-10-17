3c186dd5285198998909b0944993dc9798183937
mayar <mirohosni5@gmail.com>
2025-10-17 23:42:36 +0300

    auto: src\lab4\EmployeeUserDatabase.java

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -3,13 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class EmployeeUserDatabase {
-    private ArrayList<EmployeeUser> records;
-    private  String filename;
-    //constructor
-    public EmployeeUserDatabase(String filename){
-        this.filename=filename;
-       this.records=new ArrayList<>();
-    }
+
 
 
 
