524477bd7c1c8b228078ee503ed4db986fdc750b
mayar <mirohosni5@gmail.com>
2025-10-17 23:50:01 +0300

    auto: src\lab4\IDtkenRecord.java

--- src/lab4/IDtkenRecord.java
+++ src/lab4/IDtkenRecord.java
@@ -1,8 +1,8 @@
 package lab4;
-
+import
 public abstract class IDtkenRecord  implements recordInterfaces{
     //to check for same ids
     public static boolean isIDtaken(String key, List<recordInterfaces>list){
-        
+
     }
 }
