feed5f95a1d8056fe35c8817a00dc7eb11a3a8e4
mayar <mirohosni5@gmail.com>
2025-10-18 00:03:10 +0300

    auto: src\lab4\IDtkenRecord.java

--- src/lab4/IDtkenRecord.java
+++ src/lab4/IDtkenRecord.java
@@ -1,6 +1,6 @@
 package lab4;
 import java.util.*;
-//this will be the parent class that the rest of the classes 
+//this will be the parent class that the rest of the classes (that will need unique 
 public abstract class IDtkenRecord  implements recordInterfaces{
     //to check for same ids
     public static boolean isIDtaken(String key, List<recordInterfaces>list){
