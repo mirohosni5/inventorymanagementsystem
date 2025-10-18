ae5ef84d9e0455efa3cfc96f988faf8c4928693c
mayar <mirohosni5@gmail.com>
2025-10-19 02:01:59 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 // i used casting here instead of generics
-public abstract class databases  extend {
+public abstract class databases extends {
 
     public ArrayList<Object> records = new ArrayList<>();
     public String filename;
