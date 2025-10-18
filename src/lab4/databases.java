7d616e0b479b26a8ffc7509c5002e7cdf3997ff7
mayar <mirohosni5@gmail.com>
2025-10-19 02:24:56 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -3,7 +3,7 @@ package lab4;
 import java.io.*;
 import java.util.*;
 
-// i used casting here instead of generics i a
+// i used casting here instead of generics i also removed the parent idtaken since it was just 
 public abstract class databases {
 
     public ArrayList<Object> records = new ArrayList<>();
