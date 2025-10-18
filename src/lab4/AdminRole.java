347db74c20445fd32055cc5d7a26c4ffc14c3542
Rana Mohamed <es.rana.mohamed2023@alexu.edu.eg>
2025-10-18 22:59:13 +0300

    adminrole class

--- src/lab4/AdminRole.java
+++ src/lab4/AdminRole.java
@@ -1,9 +1,40 @@
 package lab4;
+
+import java.util.ArrayList;
+
 public class AdminRole {
      private EmployeeUserDatabase database;
     public AdminRole(){
         database = new EmployeeUserDatabase("Employees.txt");
         database.readFromFile();
     }
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        if (!database.contains(employeeId)) {
+            EmployeeUser emp = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+            database.insertRecord(emp);
+            System.out.println("Employee added successfully!");
+        } else {
+            System.out.println("Employee ID already exists!");
+        }
+    }
 
+    public EmployeeUser[] getListOfEmployees() {
+        ArrayList<EmployeeUser> list = database.returnAllRecords();
+        return list.toArray(new EmployeeUser[0]);
+    }
+
+    public void removeEmployee(String key) {
+        if (database.contains(key)) {
+            database.deleteRecord(key);
+            System.out.println("Employee removed successfully!");
+        } else {
+            System.out.println("Employee not found!");
+        }
+    }
+
+    public void logout() {
+        database.saveToFile();
+        System.out.println("Changes saved");
 }
+}
+
