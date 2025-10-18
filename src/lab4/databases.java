6959de61fca9b69d4cf3d543ddd321b86cb6c4b4
mayar <mirohosni5@gmail.com>
2025-10-19 02:01:25 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -66,7 +66,7 @@ public abstract class databases {
         return records;
     }
 
-    // here i am checking 
+    // here i am checking if the 
     public boolean contains(String key){
         for (Object recd : records) {
             recordInterfaces rec = (recordInterfaces) recd;
