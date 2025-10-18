4e84bf731d8b6bb74a354410a32e9826da8089de
mayar <mirohosni5@gmail.com>
2025-10-19 02:01:00 +0300

    auto: src\lab4\databases.java

--- src/lab4/databases.java
+++ src/lab4/databases.java
@@ -72,7 +72,7 @@ public abstract class databases {
             recordInterfaces rec = (recordInterfaces) recd;
             if (rec.getSearchKey().equalsIgnoreCase(key)) {
                 return true;
-            } 
+            }
         }
         return false;
     }
@@ -85,7 +85,7 @@ public abstract class databases {
             }
         }
         return null;
-    }//
+    }/
 
     public void insertRecord(recordInterfaces record){
         if (record == null) return;
