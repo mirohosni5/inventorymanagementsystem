52adfe545937581d049318b8ad51ecedbc0ee2d9
mayar <mirohosni5@gmail.com>
2025-10-19 22:23:28 +0300

    auto: src\lab4\EmployeeRole.java

--- src/lab4/EmployeeRole.java
+++ src/lab4/EmployeeRole.java
@@ -24,7 +24,7 @@ public class EmployeeRole implements roleinterface {
         System.out.println("Product added.");
     }
     public Product[] getListOfProducts(){
-        ArrayList<Product> list = productsDatabase.returnAllRecords();
+        ArrayList<Prod> list = productsDatabase.returnAllRecords();
         return list.toArray(new Product[0]);
 
     }
