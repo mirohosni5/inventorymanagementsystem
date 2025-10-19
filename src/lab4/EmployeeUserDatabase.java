97dc0d87b6ac3037c82c4fd8b0d7de32c301b3c6
Aya-Gebril <aya2gebril@gmail.com>
2025-10-19 11:47:02 +0300

    2nd class

--- src/lab4/EmployeeUserDatabase.java
+++ src/lab4/EmployeeUserDatabase.java
@@ -9,6 +9,19 @@ public class EmployeeUserDatabase {
     public EmployeeUserDatabase(String filename) {
         this.filename = filename;
         this.records = new ArrayList<>();
+
+        public EmployeeUser createRecordFrom(String line) {
+            String[] parts = line.split(",");
+            String id = parts[0];
+            String name = parts[1];
+            String email = parts[2];
+            String address = parts[3];
+            String phone = parts[4];
+
+            return new EmployeeUser(id, name, email, address, phone);
+        }
+
+
     }
 
     }
