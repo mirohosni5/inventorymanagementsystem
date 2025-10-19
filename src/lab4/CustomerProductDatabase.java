e01be3b0f77776c438a50b6bd3c7b40270ee4ff0
Aya-Gebril <aya2gebril@gmail.com>
2025-10-20 00:35:47 +0300

    CustomerProductDatabase

--- src/lab4/CustomerProductDatabase.java
+++ src/lab4/CustomerProductDatabase.java
@@ -1,6 +1,7 @@
 package lab4;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CustomerProductDatabase extends databases {
     public CustomerProductDatabase(String filename) {
@@ -14,7 +15,8 @@ public class CustomerProductDatabase extends databases {
 
         String customerSSN = parts[0];
         String productID = parts[1];
-        LocalDate purchaseDate = LocalDate.parse(parts[2]);
+        DateTimeFormatter F = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate purchaseDate = LocalDate.parse(parts[2], F);
         boolean paid = Boolean.parseBoolean(parts[3]);
 
         CustomerProduct c = new CustomerProduct(customerSSN, productID, purchaseDate);
