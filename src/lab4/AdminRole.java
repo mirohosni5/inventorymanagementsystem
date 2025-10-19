6630a504e82abd9eef67e4ea306027ff01233dd2
mayar <mirohosni5@gmail.com>
2025-10-19 21:25:03 +0300

    auto: src\lab4\AdminRole.java

--- src/lab4/AdminRole.java
+++ src/lab4/AdminRole.java
@@ -2,7 +2,7 @@ package lab4;
 
 import java.util.ArrayList;
 
-public class AdminRole {
+public class AdminRole implements Role {
      private EmployeeUserDatabase database;
     public AdminRole(){
         database = new EmployeeUserDatabase("Employees.txt");
