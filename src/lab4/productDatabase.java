9fb50d27b1e4a2388097c27182f111c0dfaa92fe
Aya-Gebril <aya2gebril@gmail.com>
2025-10-19 19:34:35 +0300

    Start CustomerProductDatabase

--- src/lab4/productDatabase.java
+++ src/lab4/productDatabase.java
@@ -1,6 +1,12 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class productDatabase extends databases {
+    public productDatabase(String filename){
+        super(filename);
+    }
+
 
 
 }
