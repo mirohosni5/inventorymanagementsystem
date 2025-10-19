a2c210c06f0cb7c08bd700054d9109e4dc99ce57
mayar <mirohosni5@gmail.com>
2025-10-19 22:38:57 +0300

    auto: src\lab4\EmployeeRole.java

--- src/lab4/EmployeeRole.java
+++ src/lab4/EmployeeRole.java
@@ -81,7 +81,7 @@ public class EmployeeRole implements roleinterface {
         if (changed) customerProductDatabase.saveToFile();
         return changed;
     }
-    pubnlic void logout() {
+    public void logout() {
         productsDatabase.saveToFile();
         customerProductDatabase.saveToFile();
     }
