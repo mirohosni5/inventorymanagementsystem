9977031bdce468085947ac95602d11a3af6831f0
Aya-Gebril <aya2gebril@gmail.com>
2025-10-19 21:30:47 +0300

    ProductDatabase

--- src/lab4/CustomerProductDatabase.java
+++ src/lab4/CustomerProductDatabase.java
@@ -1,5 +1,25 @@
 package lab4;
 
-public class CustomerProductDatabase {
+import java.util.ArrayList;
+
+public class CustomerProductDatabase extends databases {
+    public CustomerProductDatabase(String filename){
+        super(filename);
+    }
+
+    @Override
+    public recordInterfaces createRecord(String line) {
+        String[] parts = line.split(",");
+        if (parts.length < 6) return null;
+
+        String productID = parts[0];
+        String productName = parts[1];
+        String manufacturerName = parts[2];
+        String supplierName = parts[3];
+        int quantity = Integer.parseInt(parts[4]);
+        float price = Float.parseFloat(parts[5]);
+
+        return new Product(productID, productName, manufacturerName, supplierName, quantity, price);
+    }
 
 }
--- src/lab4/ProductDatabase.java
+++ src/lab4/ProductDatabase.java
@@ -1,9 +1,7 @@
 package lab4;
 
-import java.util.ArrayList;
-
-public class productDatabase extends databases {
-    public productDatabase(String filename){
+public class ProductDatabase extends databases {
+    public ProductDatabase(String filename){
         super(filename);
     }
 
