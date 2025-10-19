f207e3b2e2bc9b3c759235db96a0bafa826cd0c0
mayar <mirohosni5@gmail.com>
2025-10-19 21:14:36 +0300

    auto: src\lab4\AdminRole.java

--- src/lab4/AdminRole.java
+++ src/lab4/AdminRole.java
@@ -20,7 +20,7 @@ public class AdminRole implements roleinterface
     }
 
     public EmployeeUser[] getListOfEmployees() {
-        ArrayList<Object> list=database.returnALLRecords()
+        ArrayList<Object> list=database.returnALLRecords();
         ArrayList<EmployeeUser> list = database.returnAllRecords();
         return list.toArray(new EmployeeUser[0]);
     }
